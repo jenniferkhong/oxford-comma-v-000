2709d20858d7400f900559c0bf3f5b55af4e4f9f
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-17 21:03:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.length == 3
     array[0] << ","
     array[1] << ","
-    array[2] << ", and "
+    array[2] << " and "
   else
     return array.split(",")
   end
