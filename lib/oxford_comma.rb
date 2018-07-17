c8d8650bf3a74fc5028ce75712d78254c6a6b3fe
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-17 21:02:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,12 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
-    array.split("and")
+    array.split(" and ")
   elsif array.length == 3
-    array.split("," "," "and")
+    array[0] << ","
+    array[1] << ","
+    array[2] << ", and "
   else
     return array.split(",")
+  end
+end
