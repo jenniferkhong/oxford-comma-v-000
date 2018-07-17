b2adced8b793cdae0d5a8c481684891f8d516666
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-17 21:05:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,11 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  elsif array.length >= 3
+  elsif array.length == 3
     array[0] << ","
     array[1] << ","
     array[2] << ", and "
-  else
+  elsif array.length >= 4
     return array.split(",")
   end
 end
